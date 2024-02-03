614f0f2b6477865214967724a3ba1a682161ffa6
anonyblast <gustavoiafelix@gmail.com>
2024-02-03 14:26:23 -0300

    Init repository

---
+++ aula_0001/aula_0001_01.js
@@ -0,0 +1,38 @@
+/**
+ *  - AULA 0001 -> TEMA 01 - VARIÁVEIS E CONSTANTES
+ *      - O QUE SÃO VARIÁVEIS?
+ *      - O QUE SÃO CONSTANTES?
+ *      - COMO IMPRIMIR NO TERMINAL?
+*/
+
+// - O QUE SÃO VARIÁVEIS?
+var var1 = 1;
+let var2 = 2;
+// var e let são formas de declarar variáveis
+// suas estruturas são:
+// var ou let -> palavras chave
+// var1 ou var2 -> nome da variável
+// 1 ou 2 -> valor da variável
+//  - TRUQUES QUE O JAVASCRIPT TEM :)
+var1 = '1';
+var2 = '2';
+var1 = true;
+var2 = false;
+//  ** Variável significa que o valor pode mudar
+
+
+// - O QUE SÃO CONSTANTES?
+const const1 = 3;
+const const2 = 4;
+// const é uma forma de declarar constantes
+// const -> palavras chave
+// const1 ou const2 -> nome da constante
+// 3 ou 4 -> valor da constante
+// const1 = '2'; ** CONTANTES NÃO MUDAM!!!
+
+
+// - COMO IMPRIMIR NO TERMINAL?
+console.log(var1);
+console.log(var2);
+console.log(const1);
+console.log(const2);
---
+++ aula_0001/aula_0001_ex_01.js
@@ -0,0 +1,69 @@
+/**
+ *  *** EXERCÍCIOS ***
+ *  - AULA 0001 -> TEMA 01 - VARIÁVEIS E CONSTANTES
+ *      [x] - O QUE SÃO VARIÁVEIS?
+ *      [x] - O QUE SÃO CONSTANTES?
+ *      [x] - COMO IMPRIMIR NO TERMINAL?
+ * 
+ *  NOTA : 10
+*/
+
+// - O QUE SÃO VARIÁVEIS?
+// EXERCÍCIO_0001 - DECLARAR UMA VARIÁVEL E MUDAR O VALOR DELA UMA VEZ
+// *** COMEÇO DO SEU CÓDIGO AQUI
+let resposta1 = 1;
+// *** FIM DO SEU CÓDIGO AQUI
+
+// - O QUE SÃO CONSTANTES?
+// EXERCÍCIO_0002 - DECLARAR UMA CONSTANTE
+// *** COMEÇO DO SEU CÓDIGO AQUI
+const resposta2 = 3;
+// *** FIM DO SEU CÓDIGO AQUI
+
+// - COMO IMPRIMIR NO TERMINAL?
+// EXERCÍCIO_0003 - IMPRIMIR A VARIÁVEL E A CONSTANTE
+// *** COMEÇO DO SEU CÓDIGO AQUI
+console.log(resposta1);
+console.log(resposta2);
+// *** FIM DO SEU CÓDIGO AQUI
+
+// Aqui você vai colocar a variável e a constante e também o seu nome:
+avaliador(resposta1, resposta2, 'Leandro');
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *
+ * 
+ * 
+ **/ 
+function avaliador(resposta1, resposta2, seunome) {
+    console.log(`\nPrazer, ${seunome}! Vou te avaliar agora:\n*\n*\n*`);
+    if (resposta1 != undefined && resposta2 != undefined) {
+        console.log(`${seunome}, você respondeu as duas primeiras perguntas!`);
+    } else {
+        console.log(`${seunome}, você não gosta de bolo gelado?`);
+    }
+    let pergunta1 = resposta1 != undefined ? true : false;
+    let pergunta2 = resposta2 != undefined ? true : false;
+    let nota = pergunta1 && pergunta2 ? 10 : 0;
+    if (nota == 50) {
+        console.log(`${seunome}, você se saiu bem, o bolo tá na geladeira`);
+    } else {
+        console.log(`${seunome}, você não gosta de bolo gelado?`)
+    }
+    console.log(`\n*\nSUA NOTA É ${nota}`)
+}
